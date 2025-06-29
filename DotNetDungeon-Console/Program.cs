ef7e8e1ddcf825755605a34b205a49d980411cc4
Antonio Ruben Preixens Prejano <ruben.preixens@viseo.com>
2025-06-29 09:23:55 +0200

    Add validation and randomization for dungeon settings

--- DotNetDungeon-Console/Program.cs
+++ DotNetDungeon-Console/Program.cs
@@ -9,17 +9,34 @@ var dungeonCharSettings = JsonUtil.ConvertJsonPathToModelObject(PathsConst.Dunge
 if (!worldSettings.Width.HasValue || !worldSettings.Height.HasValue)
 	throw new Exception();
 
-if (!dungeonCharSettings.Air.HasValue)
+if (!dungeonCharSettings.Air.HasValue || !dungeonCharSettings.Wall.HasValue)
+	throw new Exception();
+
+if (!dungeonSettings.RoomProbabilityMin.HasValue || !dungeonSettings.RoomProbabilityMax.HasValue)
 	throw new Exception();
 
 int height = worldSettings.Height.Value;
 int width = worldSettings.Width.Value;
+int minProbability = dungeonSettings.RoomProbabilityMin.Value;
+int maxProbability = dungeonSettings.RoomProbabilityMax.Value;
 char airChar = dungeonCharSettings.Air.Value;
+char wallChar = dungeonCharSettings.Wall.Value;
 
 char[,] dungeonMatrix = new char[height, width];
 
 for (int y = 0; y < height; y++)
+{
 	for (int x = 0; x < width; x++)
-		dungeonMatrix[y, x] = airChar;
+	{
+		char cellChar = airChar;
+
+		int randomValue = MathUtil.GenerateRandomInteger(1, maxProbability);
+
+		if (randomValue <= minProbability)
+			cellChar = wallChar;
+
+		dungeonMatrix[y, x] = cellChar;
+	}
+}
 
 OutputUtil.PrintMatrix(dungeonMatrix);
