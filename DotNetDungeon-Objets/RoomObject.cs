56647f90a19a715ff118fdd4706aaa966264f65d
ERTYNEA <106114029+ERTYNEA@users.noreply.github.com>
2025-08-09 00:17:29 +0200

    Update RoomObject.cs

--- DotNetDungeon-Objets/RoomObject.cs
+++ DotNetDungeon-Objets/RoomObject.cs
@@ -7,6 +7,7 @@ public class RoomObject
 	public int Height { get; }
 	public int Width { get; }
 
+	// TODO - COPILOT: We need to put a summary here that indicates what this method does
 	public RoomObject(int y, int x, int height, int width)
 	{
 		Y = y;
