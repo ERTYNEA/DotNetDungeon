a31fdcab5167477117ebdfbe81421daddab93ded
ERTYNEA <106114029+ERTYNEA@users.noreply.github.com>
2025-08-09 00:13:55 +0200

    Update MathUtil.cs

--- DotNetDungeon-Utils/MathUtil.cs
+++ DotNetDungeon-Utils/MathUtil.cs
@@ -4,13 +4,17 @@ using System;
 
 public static class MathUtil
 {
+	// TODO - COPILOT: We need to put a summary here that indicates what this method does
 	public static int GenerateRandomInteger(int min, int max)
 	{
+		// TODO - COPILOT: We need to explain what this part of the code does
 		if (min > max)
 			throw new Exception();
 
+		// TODO - COPILOT: We need to explain what this part of the code does
 		Random random = new Random();
 
+		// TODO - COPILOT: We need to explain what this part of the code does
 		return random.Next(min, max + 1);
 	}
 }
