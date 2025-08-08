57ae2da65526991fc298bb731230a77e3248276b
ERTYNEA <ERTYNEA@hotmail.com>
2025-08-08 16:32:08 +0000

    refactor room clustering

--- DotNetDungeon-Services/Services/GameService.cs
+++ DotNetDungeon-Services/Services/GameService.cs
@@ -82,83 +82,97 @@ public class GameService : IGameService
 				for (int x = room.X; x < room.X + room.Width; x++)
 					dungeonLevelMatrix[y, x] = floorChar;
 
-		List<List<RoomObject>> islands = new List<List<RoomObject>>();
+		// Create lists to track room clusters for ensuring dungeon connectivity
 		List<RoomObject> remainingRooms = new List<RoomObject>(rooms);
+		List<List<RoomObject>> roomsClusters = new List<List<RoomObject>>();
 
+		// Group connected rooms into clusters of interconnected rooms
 		while (remainingRooms.Count > 0)
 		{
-			List<RoomObject> currentIsland = new List<RoomObject>();
+			// Create a new cluster starting with the first available room
+			List<RoomObject> currentRoomsCluster = new List<RoomObject>();
 			RoomObject startRoom = remainingRooms[0];
-			currentIsland.Add(startRoom);
+			currentRoomsCluster.Add(startRoom);
 			remainingRooms.RemoveAt(0);
 
-			bool addedRoomToIsland;
+			// Flag to track whether any room was added to the current cluster in each iteration
+			bool addedRoomToCluster;
 
+			// Repeatedly scan for rooms that connect to the current cluster until no more are found
 			do
 			{
-				addedRoomToIsland = false;
+				// Reset flag at the start of each iteration of the cluster growth process
+				addedRoomToCluster = false;
 
+				// Iterate through remaining rooms in reverse to safely remove items while iterating
 				for (int i = remainingRooms.Count - 1; i >= 0; i--)
 				{
+					// Get the current room to evaluate for adjacency with the existing cluster
 					RoomObject roomToCheck = remainingRooms[i];
 
-					foreach (RoomObject islandRoom in currentIsland)
+					// Compare the room against each room in the current cluster to check for adjacency
+					foreach (RoomObject clusterRoom in currentRoomsCluster)
 					{
+						// Check if rooms are adjacent horizontally with any vertical overlap
 						bool horizontalConnection =
-							(roomToCheck.X + roomToCheck.Width == islandRoom.X || islandRoom.X + islandRoom.Width == roomToCheck.X) &&
-							((roomToCheck.Y >= islandRoom.Y && roomToCheck.Y < islandRoom.Y + islandRoom.Height) ||
-							(roomToCheck.Y + roomToCheck.Height > islandRoom.Y && roomToCheck.Y + roomToCheck.Height <= islandRoom.Y + islandRoom.Height) ||
-							(islandRoom.Y >= roomToCheck.Y && islandRoom.Y < roomToCheck.Y + roomToCheck.Height) ||
-							(islandRoom.Y + islandRoom.Height > roomToCheck.Y && islandRoom.Y + islandRoom.Height <= roomToCheck.Y + roomToCheck.Height));
+							(roomToCheck.X + roomToCheck.Width == clusterRoom.X || clusterRoom.X + clusterRoom.Width == roomToCheck.X) &&
+							((roomToCheck.Y >= clusterRoom.Y && roomToCheck.Y < clusterRoom.Y + clusterRoom.Height) ||
+							(roomToCheck.Y + roomToCheck.Height > clusterRoom.Y && roomToCheck.Y + roomToCheck.Height <= clusterRoom.Y + clusterRoom.Height) ||
+							(clusterRoom.Y >= roomToCheck.Y && clusterRoom.Y < roomToCheck.Y + roomToCheck.Height) ||
+							(clusterRoom.Y + clusterRoom.Height > roomToCheck.Y && clusterRoom.Y + clusterRoom.Height <= roomToCheck.Y + roomToCheck.Height));
 
+						// Check if rooms are adjacent vertically with any horizontal overlap
 						bool verticalConnection =
-							(roomToCheck.Y + roomToCheck.Height == islandRoom.Y || islandRoom.Y + islandRoom.Height == roomToCheck.Y) &&
-							((roomToCheck.X >= islandRoom.X && roomToCheck.X < islandRoom.X + islandRoom.Width) ||
-							(roomToCheck.X + roomToCheck.Width > islandRoom.X && roomToCheck.X + roomToCheck.Width <= islandRoom.X + islandRoom.Width) ||
-							(islandRoom.X >= roomToCheck.X && islandRoom.X < roomToCheck.X + roomToCheck.Width) ||
-							(islandRoom.X + islandRoom.Width > roomToCheck.X && islandRoom.X + islandRoom.Width <= roomToCheck.X + roomToCheck.Width));
+							(roomToCheck.Y + roomToCheck.Height == clusterRoom.Y || clusterRoom.Y + clusterRoom.Height == roomToCheck.Y) &&
+							((roomToCheck.X >= clusterRoom.X && roomToCheck.X < clusterRoom.X + clusterRoom.Width) ||
+							(roomToCheck.X + roomToCheck.Width > clusterRoom.X && roomToCheck.X + roomToCheck.Width <= clusterRoom.X + clusterRoom.Width) ||
+							(clusterRoom.X >= roomToCheck.X && clusterRoom.X < roomToCheck.X + roomToCheck.Width) ||
+							(clusterRoom.X + clusterRoom.Width > roomToCheck.X && clusterRoom.X + clusterRoom.Width <= roomToCheck.X + roomToCheck.Width));
 
+						// If a connection exists, add the room to the current cluster and mark it for removal
 						if (horizontalConnection || verticalConnection)
 						{
-							currentIsland.Add(roomToCheck);
+							currentRoomsCluster.Add(roomToCheck);
 							remainingRooms.RemoveAt(i);
-							addedRoomToIsland = true;
+							addedRoomToCluster = true;
 							break;
 						}
 					}
 				}
-			} while (addedRoomToIsland);
+			} while (addedRoomToCluster);
 
-			islands.Add(currentIsland);
+			// Add the completed cluster to the list of room clusters
+			roomsClusters.Add(currentRoomsCluster);
 		}
 
-		if (islands.Count > 1)
+		// Connect all separate room clusters with L-shaped corridors to ensure full dungeon traversability
+		if (roomsClusters.Count > 1)
 		{
-			List<(int islandIndex, RoomObject representativeRoom)> islandRepresentatives = new List<(int, RoomObject)>();
+			List<(int clusterIndex, RoomObject representativeRoom)> clusterRepresentatives = new List<(int, RoomObject)>();
 
-			for (int i = 0; i < islands.Count; i++)
+			for (int i = 0; i < roomsClusters.Count; i++)
 			{
-				islandRepresentatives.Add((i, islands[i][0]));
+				clusterRepresentatives.Add((i, roomsClusters[i][0]));
 			}
 
-			HashSet<int> connectedIslands = new HashSet<int>();
-			connectedIslands.Add(0);
+			HashSet<int> connectedClusters = new HashSet<int>();
+			connectedClusters.Add(0);
 
-			while (connectedIslands.Count < islands.Count)
+			while (connectedClusters.Count < roomsClusters.Count)
 			{
-				int closestIsland1 = -1;
-				int closestIsland2 = -1;
+				int closestCluster1 = -1;
+				int closestCluster2 = -1;
 				int shortestDistance = int.MaxValue;
 
-				foreach (int connectedIndex in connectedIslands)
+				foreach (int connectedIndex in connectedClusters)
 				{
-					for (int i = 0; i < islandRepresentatives.Count; i++)
+					for (int i = 0; i < clusterRepresentatives.Count; i++)
 					{
-						if (connectedIslands.Contains(i))
+						if (connectedClusters.Contains(i))
 							continue;
 
-						RoomObject room1 = islandRepresentatives[connectedIndex].representativeRoom;
-						RoomObject room2 = islandRepresentatives[i].representativeRoom;
+						RoomObject room1 = clusterRepresentatives[connectedIndex].representativeRoom;
+						RoomObject room2 = clusterRepresentatives[i].representativeRoom;
 
 						int centerX1 = room1.X + room1.Width / 2;
 						int centerY1 = room1.Y + room1.Height / 2;
@@ -170,16 +184,16 @@ public class GameService : IGameService
 						if (distance < shortestDistance)
 						{
 							shortestDistance = distance;
-							closestIsland1 = connectedIndex;
-							closestIsland2 = i;
+							closestCluster1 = connectedIndex;
+							closestCluster2 = i;
 						}
 					}
 				}
 
-				if (closestIsland1 != -1 && closestIsland2 != -1)
+				if (closestCluster1 != -1 && closestCluster2 != -1)
 				{
-					RoomObject room1 = islandRepresentatives[closestIsland1].representativeRoom;
-					RoomObject room2 = islandRepresentatives[closestIsland2].representativeRoom;
+					RoomObject room1 = clusterRepresentatives[closestCluster1].representativeRoom;
+					RoomObject room2 = clusterRepresentatives[closestCluster2].representativeRoom;
 
 					int centerX1 = room1.X + room1.Width / 2;
 					int centerY1 = room1.Y + room1.Height / 2;
@@ -225,7 +239,7 @@ public class GameService : IGameService
 						}
 					}
 
-					connectedIslands.Add(closestIsland2);
+					connectedClusters.Add(closestCluster2);
 				}
 			}
 		}
