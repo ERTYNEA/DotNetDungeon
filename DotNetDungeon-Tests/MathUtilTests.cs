0341246e0b01689818c8d507ff10f595460c6a20
Antonio Ruben Preixens Prejano <ruben.preixens@viseo.com>
2025-08-10 23:17:51 +0200

    Add MSTest support and new tests for MathUtil

---
+++ DotNetDungeon-Tests/MathUtilTests.cs
@@ -0,0 +1,51 @@
+﻿namespace DotNetDungeon_Tests;
+
+using DotNetDungeon_Utils;
+using FluentAssertions;
+
+[TestClass]
+public sealed class MathUtilTests
+{
+	[TestMethod]
+	public void GenerateRandomInteger_min_is_greater_than_max()
+	{
+		// Arrange
+		int min = 2;
+		int max = 1;
+
+		// Act
+		Action act = () => MathUtil.GenerateRandomInteger(min, max);
+
+		// Assert
+		act.Should().Throw<Exception>();
+	}
+
+	[TestMethod]
+	public void GenerateRandomInteger_min_is_equal_to_max()
+	{
+		// Arrange
+		int min = 1;
+		int max = 1;
+
+		// Act
+		int result = MathUtil.GenerateRandomInteger(min, max);
+
+		// Assert
+		result.Should().Be(min);
+	}
+
+	[TestMethod]
+	public void GenerateRandomInteger_min_is_less_than_max()
+	{
+		// Arrange
+		int min = 1;
+		int max = 2;
+
+		// Act
+		int result = MathUtil.GenerateRandomInteger(min, max);
+
+		// Assert
+		result.Should().BeGreaterThanOrEqualTo(min);
+		result.Should().BeLessThanOrEqualTo(max);
+	}
+}
